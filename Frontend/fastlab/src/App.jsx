f8e422a89842f28795fc5e0d2236f3856b80200a
GNNZVL19 <gonzavolante@gmail.com>
2025-01-14 01:38:27 -0300

    Ingresar pedido en progreso

--- Frontend/fastlab/src/App.jsx
+++ Frontend/fastlab/src/App.jsx
@@ -6,13 +6,17 @@ import PacienteInicio from './pages/PacienteInicio'
 import PacienteHistorial from './pages/PacienteHistorial'
 import PacienteHistorialResultadoAnalisis from './pages/PacienteHistorialResultadoAnalisis'
 import PacienteMisDatos from './pages/PacienteMisDatos'
-import AdminPedidos from './pages/AdminPedidos/AdminPedidos'
+
 import AdminResultados from './pages/AdminResultados/AdminResultados'
 import AdminConfiguracion from './pages/AdminConfiguracion/AdminConfiguracion'
 import AdminConfiguracionDoctores from './pages/AdminConfiguracion/AdminConfiguracionDoctores'
 import AdminConfiguracionAnadirDoctores from './pages/AdminConfiguracion/AdminConfiguracionAnadirDoctores'
 import AdminConfiguracionEditarDoctores from './pages/AdminConfiguracion/AdminConfiguracionEditarDoctores'
 import AdminConfiguracionMiCuenta from './pages/AdminConfiguracion/AdminConfiguracionMiCuenta'
+
+
+import SearchPatient from "./pages/AdminPedidos/SearchPatient";
+import SelectionPatient from "./pages/AdminPedidos/SelectionPatient";
 
 function App() {
     return (
@@ -29,13 +33,16 @@ function App() {
                     <Route path='/paciente/misdatos' element={<PacienteMisDatos />} />
 
                     {/* rutas para el admin */}
-                    <Route path='/admin/pedidos' element={<AdminPedidos />} />
+                    
                     <Route path='/admin/resultados' element={<AdminResultados />} />
                     <Route path='/admin/configuracion' element={<AdminConfiguracion />} />
                     <Route path='/admin/configuracion/doctores' element={<AdminConfiguracionDoctores />} />
                     <Route path='/admin/configuracion/doctores/añadir' element={<AdminConfiguracionAnadirDoctores />} />
                     <Route path='/admin/configuracion/doctores/:id' element={<AdminConfiguracionEditarDoctores />} />
                     <Route path='/admin/configuracion/mi-cuenta' element={<AdminConfiguracionMiCuenta />} />
+                    <Route path="/admin/ingresar-orden" element={<SelectionPatient />}/>
+                    <Route path="/admin/ingresar-orden/paciente-registrado"element={<SearchPatient />}/>
+                    
                 </Routes>
             </Router>
         </>
