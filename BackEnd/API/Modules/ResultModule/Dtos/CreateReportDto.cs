8ddfd444f6b76efd65dfbe96a641323e58e8e701
Lucas Pirez <lucaspirez42@gmail.com>
2025-01-15 09:47:17 -0300

    this commit add changes in Report Entity

--- BackEnd/API/Modules/ResultModule/Dtos/CreateReportDto.cs
+++ BackEnd/API/Modules/ResultModule/Dtos/CreateReportDto.cs
@@ -3,10 +3,13 @@ namespace API.Modules.ResultModule.Dtos
 {
     public class CreateReportDto
     {
-
         public required string Status { get; set; }
 
         public DateTime DateExam { get; set; }
+
+        public string? Priority { get; set; }
+
+        public string? Observations { get; set; }
 
         public int PatientId { get; set; }
 
