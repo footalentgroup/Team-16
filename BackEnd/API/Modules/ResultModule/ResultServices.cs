364554603c35f724cb813c433da356fef2444bc0
Lucas Pirez <lucaspirez42@gmail.com>
2025-01-16 11:11:30 -0300

    this commit add changes in result module

--- BackEnd/API/Modules/ResultModule/ResultServices.cs
+++ BackEnd/API/Modules/ResultModule/ResultServices.cs
@@ -71,6 +71,7 @@ namespace API.Modules.ResultModule
 
         public async Task<Report> CreateOrder(CreateReportDto createReportDto)
         {
+            createReportDto.ExamIds = createReportDto?.ExamIds ?? new List<int>();
 
             var report = _mapper.Map<Report>(createReportDto);
 
@@ -95,55 +96,6 @@ namespace API.Modules.ResultModule
             {
                 return ServiceResult<object>.FailedResult(StatusCodes.Status500InternalServerError, ex.Message + ex?.InnerException);
             }
-        }
-
-        public async Task<ServiceResult<List<ResponseReportDto>>> GetManyByPatientIdAsync(int patientId)
-        {
-
-            var result = await _context
-                                    .Reports
-                                    .Where(x => x.PatientId == patientId)
-                                    .Include(x => x.Results)
-                                    .ThenInclude(r => r.Parameter)
-                                    .Select(x => new
-                                    {
-                                        x.DateExam,
-                                        x.Doctor,
-                                        ExamIds = x.ExamIds.ToList(),
-                                        x.Id,
-                                        x.Patient,
-                                        x.Status,
-                                        Results = x.Results.ToList()
-                                    }).ToListAsync();
-
-            if (dic == null)
-            {
-                dic = await _context.Parameters.Include(x => x.Exam).ToDictionaryAsync(parameter => parameter.Id, param => param.Exam.Name);
-            }
-
-            var response = result.Select(x => new ResponseReportDto()
-            {
-                DateExam = x.DateExam,
-                Doctor = x.Doctor,
-                ExamIds = x.ExamIds,
-                Id = x.Id,
-                Patient = x.Patient,
-                Status = x.Status,
-                Results = x.Results.Select(x =>
-                {
-                    var param = x.Parameter;
-                    string examName = GetExamByParameterId(param.Id);
-
-                    return CheckTypeResult.Check(x, param, examName);
-                }).ToList()
-            }).ToList();
-
-            if (result == null)
-            {
-                return ServiceResult<List<ResponseReportDto>>.FailedResult(StatusCodes.Status500InternalServerError, "Lista de roportes no encontrada");
-            }
-
-            return ServiceResult<List<ResponseReportDto>>.SuccessResult(response);
         }
 
         public async Task<ServiceResult<UpdateResultDto>> UpdateResultDto(UpdateResultDto dto)
@@ -204,23 +156,89 @@ namespace API.Modules.ResultModule
 
         }
 
+        public async Task<ServiceResult<List<ResponseReportDto>>> GetManyByPatientIdAsync(int patientId)
+        {
+
+            var result = await _context
+                                    .Reports
+                                    .Where(x => x.PatientId == patientId)
+                                    .Include(x => x.Results)
+                                    .ThenInclude(r => r.Parameter)
+                                    .Select(x => new
+                                    {
+                                        x.DateExam,
+                                        x.Doctor,
+                                        x.ExamIds,
+                                        x.Id,
+                                        x.Patient,
+                                        x.Status,
+                                        x.Priority,
+                                        x.Observations,
+                                        Results = x.Results.ToList()
+                                    }).ToListAsync();
+
+            if (dic == null)
+            {
+                dic = await _context.Parameters.Include(x => x.Exam).ToDictionaryAsync(parameter => parameter.Id, param => param.Exam.Name);
+            }
+
+            var response = result.Select(x => new ResponseReportDto()
+            {
+                DateExam = x.DateExam,
+                Doctor = x.Doctor,
+                ExamIds = x.ExamIds.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(),
+                Id = x.Id,
+                Patient = x.Patient,
+                Status = x.Status,
+                Priority = x.Priority,
+                Observations = x.Observations,
+                Results = x.Results.Select(x =>
+                {
+                    var param = x.Parameter;
+                    string examName = GetExamByParameterId(param.Id);
+
+                    return CheckTypeResult.Check(x, param, examName);
+                }).ToList()
+            }).ToList();
+
+            if (result == null)
+            {
+                return ServiceResult<List<ResponseReportDto>>.FailedResult(StatusCodes.Status500InternalServerError, "Lista de roportes no encontrada");
+            }
+
+            return ServiceResult<List<ResponseReportDto>>.SuccessResult(response);
+        }
+
+
         public async Task<ServiceResult<List<ReportResponseWithoutResultsDto>>> GetAll()
         {
             try
             {
-                var response = await _context.Reports.Select(x => new ReportResponseWithoutResultsDto()
+                var response = await _context.Reports.Select(x => new
                 {
                     DateExam = x.DateExam,
                     Doctor = x.Doctor,
-                    ExamIds = x.ExamIds,
+                    ExamIds = x.ExamIds ?? "",
+                    Id = x.Id,
+                    Observations = x.Observations ?? "",
+                    Patient = x.Patient,
+                    Priority = x.Priority ?? "",
+                    Status = x.Status ?? "",
+                }).ToListAsync();
+
+                var responseDto = response.Select(x => new ReportResponseWithoutResultsDto()
+                {
+                    DateExam = x.DateExam,
+                    Doctor = x.Doctor,
+                    ExamIds = x.ExamIds.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(),
                     Id = x.Id,
                     Observations = x.Observations,
                     Patient = x.Patient,
                     Priority = x.Priority,
                     Status = x.Status,
-                }).ToListAsync();
+                }).ToList();
 
-                return ServiceResult<List<ReportResponseWithoutResultsDto>>.SuccessResult(response);
+                return ServiceResult<List<ReportResponseWithoutResultsDto>>.SuccessResult(responseDto);
 
             }
             catch (Exception ex)
@@ -236,6 +254,7 @@ namespace API.Modules.ResultModule
                 var result = await _context
                                .Reports
                                .Where(x => x.Id == reportId)
+                               .Include(x => x.Doctor)
                                .Include(x => x.Results)
                                .ThenInclude(r => r.Parameter)
                                .ThenInclude(r => r.Exam)
@@ -243,10 +262,12 @@ namespace API.Modules.ResultModule
                                {
                                    x.DateExam,
                                    x.Doctor,
-                                   ExamIds = x.ExamIds.ToList(),
+                                   x.ExamIds,
                                    x.Id,
                                    x.Patient,
                                    x.Status,
+                                   x.Priority,
+                                   x.Observations,
                                    Results = x.Results.ToList()
                                }).FirstOrDefaultAsync();
 
@@ -257,7 +278,6 @@ namespace API.Modules.ResultModule
 
                 if (dic == null)
                 {
-                    Console.WriteLine("hol parameter busca ------------------------");
                     dic = await _context.Parameters.Include(x => x.Exam).ToDictionaryAsync(parameter => parameter.Id, param => param.Exam.Name);
                 }
 
@@ -265,10 +285,12 @@ namespace API.Modules.ResultModule
                 {
                     DateExam = result.DateExam,
                     Doctor = result.Doctor,
-                    ExamIds = result.ExamIds,
+                    ExamIds = result.ExamIds.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList(),
                     Id = result.Id,
                     Patient = result.Patient,
                     Status = result.Status,
+                    Priority = result.Priority,
+                    Observations = result.Observations,
                     Results = result.Results.Select(x =>
                     {
                         var param = x.Parameter;
@@ -287,6 +309,42 @@ namespace API.Modules.ResultModule
 
             }
         }
+
+        public async Task<ServiceResult<object>> UpdateReport(UpdateReportDto updateDto)
+        {
+            try
+            {
+                var report = await _context.Reports.FirstOrDefaultAsync(report => report.Id == updateDto.Id);
+
+                if (report == null)
+                {
+                    return ServiceResult<object>.FailedResult(StatusCodes.Status404NotFound, "Orden No encontrado");
+                }
+                var updatedReport = new Report()
+                {
+                    Id = report.Id,
+                    Priority = updateDto?.Priority ?? report.Priority,
+                    Observations = updateDto?.Observations ?? report.Observations,
+                    Status = updateDto?.Status ?? report.Status,
+                    PatientId = updateDto?.PatientId ?? report.PatientId,
+                    ExamIds = updateDto?.ExamIds != null ? string.Join(",", updateDto.ExamIds) : report.ExamIds,
+                    DateExam = updateDto?.DateExam ?? report.DateExam,
+                    DoctorId = updateDto?.DoctorId ?? report.DoctorId,
+                };
+
+
+                _context.Entry(report).CurrentValues.SetValues(updatedReport);
+                await _context.SaveChangesAsync();
+
+                return ServiceResult<object>.SuccessResult(updatedReport);
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult<object>.FailedResult(StatusCodes.Status500InternalServerError, ex.Message + ex?.InnerException);
+
+            }
+        }
+
 
         private string GetExamByParameterId(int parameterId)
         {
